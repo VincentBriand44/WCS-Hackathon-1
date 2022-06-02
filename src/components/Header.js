84ba29327f153921e58a1e9bed0c9fb2836d3297
David <104822295+Dav-Chung@users.noreply.github.com>
2022-06-02 14:44:15 +0200

    maj header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,10 @@
 const Header = () => {
-  return <div></div>;
+  return (
+  <div class ="header">
+    <span class="headtext"> Star Love</span>
+    <img src="image/**" class="logo" alt="my-logo" />
+  </div>
+  );
 };
 
 export default Header;
