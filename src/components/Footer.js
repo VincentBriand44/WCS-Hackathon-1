84aea31985a84dc6d8cf693f811de765716c719d
Jonas Moreau <jonas.moreau@hotmail.com>
2022-06-02 14:38:43 +0200

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+import swipImg from "../styles/img/footer/swip.png"
+import matchesImg from "../styles/img/footer/coeur.png"
+import profilImg from "../styles/img/footer/profil.png"
+import settingsImg from "../styles/img/footer/settings.png"
+
 const Footer = () => {
-  return <div></div>;
+  return (
+    <nav className="footer"> 
+      <Link to="/app"> <img src={swipImg} alt=""/></Link>
+      <Link to="/app/matches"><img src={matchesImg} alt=""/></Link>
+      <Link to="/app/profil"><img src={profilImg} alt=""/></Link>
+      <Link to="/app/settings"><img src={settingsImg} alt=""/></Link>
+    </nav>
+
+);
+
 };
 
 export default Footer;
