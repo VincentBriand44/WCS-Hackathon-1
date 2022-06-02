ecdc09bb8d2b103e9dd8dc1934125c998351ca4d
David <104822295+Dav-Chung@users.noreply.github.com>
2022-06-02 22:15:09 +0200

    made home page

---
+++ src/components/HomeButtons.js
@@ -0,0 +1,13 @@
+const HomeButtons = () => {
+    return (
+      <div className="homeButtons">
+        < a className="button" href=""><strong>Creer un compte</strong></a>
+        
+        < a className="button" href=""><strong>Connexion</strong></a>
+       
+      </div>
+    );
+  };
+  
+  export default HomeButtons;
+  
