b57d1627f2318a051e9b252d9225067243202568
Alexandra Panastier <panastier.alexandra@gmail.com>
2022-06-03 02:52:16 +0200

    News changement

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import SwipeProfile from "./components/SwipeProfile";
 import Home from "./pages/Home";
 import Swipe from "./pages/Swipe";
+import ItsMatch from "./pages/ItsMatch";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="app" element={<Swipe />} />
-       
+        <Route path="/app/itsmatch" element={<ItsMatch />} />
       </Routes>
     </div>
   );
