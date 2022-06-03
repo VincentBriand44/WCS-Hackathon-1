43348b9696e37112f64d36999f5d3ae028aac513
Jonas Moreau <jonas.moreau@hotmail.com>
2022-06-03 13:17:49 +0200

    add profil

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -23,11 +23,11 @@ function changeDescri(){
   document.getElementById("descri").innerHTML=descri_id;
 }
 function changePosition(){
-  let pos_id= prompt("Entrez votre description", "votre description ?");
+  let pos_id= prompt("Entrez votre positio nde recherche", "votre lieu de recherche ?");
   document.getElementById("pos").innerHTML=pos_id;
 }
 function changePassion(){
-  let passion_id= prompt("Entrez votre description", "votre description ?");
+  let passion_id= prompt("Entrez vos passions", "vos passions?");
   document.getElementById("passion").innerHTML=passion_id;
 }
 
@@ -40,30 +40,30 @@ const Profile = () => {
         <img src={ProfImg} alt=""/>
         <div className="profileText">
           <h2> 
-            <span id="name">Bly (CC-5052)</span> 
-            (<span id="age">53 </span>.ans)
+            <span id="name"> Bly Bee</span> 
+            (<span id="age"> 53 </span>.ans)
             <button onClick={changeName}><img src={settingsImg} alt=""/></button>
           </h2> 
           <h3>
             <span>Genre:</span> 
             <button onClick={changeGenre}><img src={settingsImg} alt=""/></button>
           </h3> 
-          <p id="genre"> Homme</p>
+          <p id="genre"> Féminin </p>
 
           <h3>Affiliations:<button onClick={changeAffiliation}><img src={settingsImg} alt=""/></button></h3>
           <p id="affiliations">République galactique
           </p>
           <h3>A propos:<button onClick={changeDescri}><img src={settingsImg} alt=""/></button></h3>
-          <p id="descri">  Figurant parmi les innombrables soldats clones développés et entraînés sur Kamino pour servir dans les rangs de la République Galactique pour contrer la menace de la Confédération des Systèmes Indépendants, le Commandant clone Bly se distinguait de la masse des clones. Il était notamment l'un des rares à posséder un nom et pas seulement un matricule, mais aussi à avoir développé au cours de la guerre un système de pensée plus autonome. Bly faisait partie des commandants clones qui reçurent le dur entraînement des Commandos de Reconnaissance Avancée, sous la houlette du commando Alpha-17, et il était par conséquent un soldat aguerri et capable de faire face à toutes les situations, contrairement à la plupart des soldats clones qui n'ont qu'une liberté de pensée très limitée, voire inexistante.
+          <p id="descri">  Veuillez cliquer sur le bouton pour compléter votre profil
           </p>
-          <h3>Position:
+          <h3>Cités:
             <button onClick={changePosition}><img src={settingsImg} alt=""/></button>
           </h3>
           <p id="pos"> Kamino </p>
           <h3>Passion:
             <button onClick={changePassion} ><img src={settingsImg} alt=""/></button>
           </h3>
-          <p id="passion" > Casser du droides, les pancakes et l'odeur du napalm au petit matin</p>
+          <p id="passion" > Veuillez cliquer sur le bouton pour compléter votre profil </p>
         </div>
       </div>
       <Footer />
